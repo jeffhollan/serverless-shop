1da4820979649cab6c3693d020385154e796c14a
Jeff Hollan <jeffhollan@outlook.com>
2017-01-31 21:51:15 -0800

    added comment page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,3 +7,26 @@ $(document).ready(
                   });
             });
       });
+
+function processForm(e) {
+      console.log(e);
+   if (e.preventDefault) e.preventDefault();
+  
+   var xhr = new XMLHttpRequest();
+//    xhr.open("POST", "http://requestb.in/1616ru71?inspect");
+//     xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+//     var item = {
+//         email: 'foo'
+//     };
+// //     document.getElementById("submission_receieved").classList.remove("hidden");
+//     xhr.send(JSON.stringify(item));
+    $('#myModal').modal('hide');
+   return false;
+}
+
+var form = document.getElementById('order_form');
+if (form.attachEvent) {
+    form.attachEvent("submit", processForm);
+} else {
+    form.addEventListener("submit", processForm);
+}
