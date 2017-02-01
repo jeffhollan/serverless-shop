d0bdd6f62df527353ea9a281aedb3d091832d534
Jeff Hollan <jeffhollan@outlook.com>
2017-02-01 14:15:40 -0800

    updating main.js in assets

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,6 +6,14 @@ $(document).ready(
                         console.log('clicked me');
                   });
             });
+
+            var quickComments = document.getElementsByClassName('quick_comment');
+            Array.prototype.forEach.call(quickComments, function (el, i) {
+                  el.addEventListener('click', function (e) {
+                        // console.log(document.getElementById(e.srcElement.id).innerHTML);
+                        callLogic(document.getElementById(e.srcElement.id).innerHTML);
+                  });
+            });
       });
 
 function processForm(e) {
@@ -23,18 +31,36 @@ function processForm(e) {
       return false;
 }
 
+function callLogic(text) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://prod-28.australiaeast.logic.azure.com:443/workflows/bd315190415646088932f99c120dbdc6/triggers/manual/run?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5usDzJmGBjCa1VnBYBK3yGdkdGV1Q4SCDgIhqrLHXuE");
+      xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+      var item = {
+            comment: text
+      };
+      xhr.onload = function (e) {
+            if (xhr.readyState === 4) {
+                  if (xhr.status === 200) {
+                        var response = JSON.parse(xhr.responseText);
+                        console.log(xhr.responseText);
+                        document.getElementById('score').innerHTML = response.score;
+                        document.getElementById('keyPhrases').innerHTML = response.phrases;
+                        document.getElementById('results').classList.remove('hidden');
+                        document.getElementById('loading').classList.add('hidden');
+                  }
+                  else {
+                        document.getElementById('loading').classList.add('hidden');
+                  }
+            }
+      };
+      xhr.send(JSON.stringify(item));
+      document.getElementById('loading').classList.remove('hidden');
+}
 function submitComment(e) {
       if (e.preventDefault) e.preventDefault();
 
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", "http://requestb.in/1616ru71?inspect");
-      xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-      var item = {
-            comment: document.getElementById('comment_form').value
-      };
-      xhr.send(JSON.stringify(item));
-      xhr.send();
-      document.getElementById('comment_form').classList.remove('hidden');
+      callLogic(document.getElementById('comment').value);
+
       return false;
 }
 
